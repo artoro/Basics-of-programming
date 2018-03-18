4754e3e7c38e9f741fc03e086df4a502ec49a244
Artur Szydłowski <artoro.pl@gmail.com>
2018-03-18 13:53:27 +0100

    Add files via upload

---
+++ Programming in CS/Csharp Lesson 1/Lab2/WindowsFormsApplication1/WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            this.BackColor = colorDialog1.Color;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            button2.BackColor = colorDialog1.Color;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            label1.ForeColor = colorDialog1.Color;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            richTextBox1.BackColor = colorDialog1.Color;
+        }
+    }
+}
