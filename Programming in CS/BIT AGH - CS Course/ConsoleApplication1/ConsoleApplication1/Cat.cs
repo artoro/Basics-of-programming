769b98383446fd6fdcab618c1cac9671d2c9bcfd
Artur Szydłowski <artoro.pl@gmail.com>
2018-03-18 14:20:49 +0100

    Add files via upload

---
+++ Programming in CS/BIT AGH - CS Course/ConsoleApplication1/ConsoleApplication1/Cat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Cat : Animal
+    {
+        private string colour;
+        private int age;
+        private float weight;
+
+        public Cat(string name, string colour, int age, float weight) : base(name) // : bazowy_konstr(args)
+        {
+            this.colour = colour;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine("Miau.. " + Name);
+        }
+
+        public void Feed(string food)
+        {
+            float mass = food.Length;
+            Console.Write(food + " -> ");
+            for (int i = 0; i < food.Length; i++)
+            {
+                Console.Write("Am.. ");
+            }
+            Meow();
+            weight += mass / 100;
+        }
+    }
+}
---
+++ Programming in CS/BIT AGH - CS Course/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //KONSOLA
+            Console.WriteLine("KOT");
+
+            //PĘTLA
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine("LUBIĘ KOTY");
+
+            //METODA
+            Console.WriteLine(WriteString());
+
+            //METODA Z WARUNKIEM
+            Console.WriteLine(TestString());
+
+            //KLASA
+            var kot = new Cat("Garfield", "Red", 2, 1.3f);
+            Console.WriteLine(kot.Name);
+            kot.Meow();
+            kot.Feed("Chrupki");
+
+            //WYŁĄCZNIK
+            Console.ReadKey();
+        }
+
+        static string WriteString()
+        {
+            string s;
+            Console.Write("ECHO: ");
+            s = Console.ReadLine();
+            return s;
+        }
+
+        static string TestString()
+        {
+            string s;
+            string p = "NIE KOT";
+            Console.Write("TEST KOTA: ");
+            s = Console.ReadLine();
+            if (s == "kot" || s == "KOT")
+            {
+                p = s;
+            }
+            return p;
+        }
+    }
+}
